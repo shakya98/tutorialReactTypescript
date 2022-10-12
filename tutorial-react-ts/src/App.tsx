40d31edc0ab11b6c628c396d70392dac354a85a0
shakya <shakya980528@gmail.com>
2022-10-12 05:00:00 -0700

    Creating Item component and displying data

--- tutorial-react-ts/src/App.tsx
+++ tutorial-react-ts/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 import { Drawer } from "@mui/material";
-import LinearProgress from "@mui/material/LinearProgress";
-import Grid from "@mui/material/Grid";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import Badge from "@mui/material/Badge";
 import { Wrapper } from "./App.styles";
+import Item from "./Item/Item";
 
 export type CartItemType = {
   id: number;
@@ -26,7 +27,23 @@ const App = () => {
     getProducts
   );
   console.log(data);
-  return <div className="App">Hellow</div>;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
---
+++ tutorial-react-ts/src/Item/Item.tsx
@@ -0,0 +1,24 @@
+import Button from "@material-ui/core/Button";
+import { type } from "@testing-library/user-event/dist/type";
+import React from "react";
+import { CartItemType } from "../App";
+import { Wrapper } from "../App.styles";
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
+  <Wrapper className="itemWrapper">
+    <img className="itemImg" src={item.image} alt={item.title} />
+    <div className="itemDiv">
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <h3>{item.price}</h3>
+    </div>
+    <Button className="addToCartBtn" onClick={() => handleAddToCart(item)}>Add to Cart</Button>
+  </Wrapper>
+);
+
+export default Item;
