0823ef2cedc9789034db45c0415fee4832d0904b
shakya <shakya980528@gmail.com>
2022-10-13 04:03:27 -0700

    Adding the add to cart button and slide in drawer

--- tutorial-react-ts/src/App.styles.ts
+++ tutorial-react-ts/src/App.styles.ts
@@ -1,3 +1,14 @@
-import styled from 'styled-components';
+import IconButton from "@material-ui/core/IconButton";
+import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  margin: 40px;
+`;
+
+export const StyledButton = styled(IconButton)`
+    z-index:100
+    text-align:right;
+    left: 100%;
+    top: 20px;
+    right:20px;
+`;
--- tutorial-react-ts/src/App.tsx
+++ tutorial-react-ts/src/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
-import { Drawer } from "@mui/material";
+import Drawer from "@material-ui/core/Drawer";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
-import Badge from "@mui/material/Badge";
-import { Wrapper } from "./App.styles";
+import Badge from "@material-ui/core/Badge";
+import { Wrapper, StyledButton } from "./App.styles";
 import Item from "./Item/Item";
 
 export type CartItemType = {
@@ -22,11 +22,15 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("https://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
   console.log(data);
+
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -35,6 +39,14 @@ const App = () => {
 
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon/>
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
